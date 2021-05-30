f764b9c422ecf1d85c82a21166c8cccf17da7d82
muralidhar <muralidharpettela@gmail.com>
2021-05-30 13:20:14 +0000

    Python modules

---
+++ example.py
@@ -0,0 +1,11 @@
+# python module
+
+def add(a, b):
+    """This program adds two numbers and return the result"""
+    result = a + b
+    return result
+
+def sub(a, b):
+    """This program subs two numbers and return the result"""
+    result = a - b
+    return result
